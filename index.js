5f3e8878687070eda1ad3d29a16203c179739f6c
Shaun Clift <scliftas@gmail.com>
2019-10-06 12:11:12 +0100

    Added comments

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+/**
+ * Store an array of actions, new actions will be appended
+ * to this array via the action() function. These actions should
+ * then be expored by a module for access by smith-api, to be 
+ * indexed ready for execution.
+ */
 const actions = [];
 
+/**
+ * Add a new action to the actions array
+ *
+ * @param {string} task Task string that the action matches to i.e. ('what is the time')
+ * @param {function} func Handler function to call when the action is executed
+ */
 const action = (task, func) => {
     actions.push({
         task: task.toLowerCase(),
