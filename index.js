a9ea9c409eadd30e0f67f43abe75f7f6d4059603
Shaun Clift <scliftas@gmail.com>
2019-10-06 12:11:01 +0100

    Removed redundant handler function

--- index.js
+++ index.js
@@ -7,22 +7,7 @@ const action = (task, func) => {
     });
 };
 
-const handler = (task) => {
-    const taskLower = task.toLowerCase();
-    const actionToDo = actions.filter((act) => taskLower.includes(act.task))[0];
-
-    let params = null;
-    const possibleParams = taskLower.split(actionToDo.task);
-
-    if (possibleParams.length > 1) {
-        params = possibleParams[1].trim();
-    }
-
-    return actionToDo.handler(params);
-};
-
 module.exports = {
-    handler,
     action,
     actions,
 }
